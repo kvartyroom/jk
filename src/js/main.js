81acde03e4d2acc3c5825e14b274a8512fb12128
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-12-03 19:27:25 +0300

    Add partially infrastructure card.

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,17 @@ $(document).ready(function() {
   owl.owlCarousel({
     loop:false,
     margin:0,
-    items: 1,
-    responsive:{
-
-    }
+    items: 1
   })
+
+  $('.card__thumb').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+    closeBtnInside: true,
+		mainClass: 'mfp-img-mobile',
+    closeMarkup: '<button type="button" class="mfp-close"></button>',
+		image: {
+			verticalFit: true
+		}
+	});
 })
